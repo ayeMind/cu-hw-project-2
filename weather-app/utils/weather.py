c4c7e5ace307a4d7a17d9aaae3ae82396106ccc6
ayeMind <fzerix@gmail.com>
2024-10-30 13:17:29 +0300

    feat: add five days predict parsing

--- weather-app/utils/weather.py
+++ weather-app/utils/weather.py
@@ -1,44 +1,72 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv()
 API_KEY = os.getenv('ACCUWEATHER_API_KEY')
-
-def parse_weather_data(weather_data):
+    
+def parse_five_days_weather(weather_data):
     """
-    Принимает прогноз погоды из API и возвращает словарь с основными параметрами погоды
-    (температура, влажность, ветер, осадки, облачность, атмосферное давление)
+    Принимает прогноз погоды из API и возвращает список из словарей с погодой на 5 дней
+    (температура, относительная влажность, вероятность осадков, скорость ветра, реальное ощущение температуры, облачность)
+    возращая среднее между минимумом и максимумом для каждого параметра
     """
     if not weather_data:
         return None
     
-    weather_data = weather_data[0]
+    def get_mean(param):
+        return (param["Minimum"]["Value"] + param["Maximum"]["Value"]) / 2
     
-    temperature = float(weather_data["Temperature"]["Metric"]["Value"]) # в градусах цельсия
-    humidity = int(weather_data["RelativeHumidity"]) # в процентах
-    wind_speed = float(weather_data["Wind"]["Speed"]["Metric"]["Value"]) # в км/ч
-    precipitation = float(weather_data["PrecipitationSummary"]["Precipitation"]["Metric"]["Value"]) # в миллиметрах
-    cloud_cover = int(weather_data["CloudCover"]) # в процентах
-    pressure = int(weather_data["Pressure"]["Metric"]["Value"]) # в миллибарах
+    def get_delta_days(param):
+        current_date = date.today()
+        date_string = param["Date"][:10]
+        param_date = date.fromisoformat(date_string)
+        delta_days = (current_date - param_date).days
+        return abs(delta_days)
     
-    return {
-        "temperature": temperature,
-        "humidity": humidity,
-        "wind_speed": wind_speed,
-        "precipitation": precipitation,
-        "cloud_cover": cloud_cover,
-        "pressure": pressure
-    }
+    def get_delta_days_text(delta_days):
+        if delta_days == 0:
+            return "Сегодня"
+        elif delta_days == 1:
+            return "Завтра"
+        elif delta_days == 2:
+            return "Послезавтра"
+        elif delta_days == 3 or delta_days == 4:
+            return f"Через {delta_days} дня"
+        else:
+            return f"Через {delta_days} дней"
     
-def get_weather(location_key):
-    url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}"
-    params = {"apikey": API_KEY, "details": "true"}
+    result_list = []
+    for day in weather_data["DailyForecasts"]:
+        temperature = get_mean(day["Temperature"])
+        real_temperature = get_mean(day["RealFeelTemperature"])
+        relative_humidity = day["Day"]["RelativeHumidity"]["Average"]
+        precipation_probability = day["Day"].get("PrecipitationProbability")
+        wind_speed = day["Day"]["Wind"]["Speed"]["Value"]
+        cloud_cover = day["Day"].get("CloudCover")
+        
+        result = {
+            "delta_days": get_delta_days_text(get_delta_days(day)),
+            "temperature": temperature,
+            "real_temperature": real_temperature,
+            "humidity": relative_humidity,
+            "precipitation": precipation_probability,
+            "wind_speed": wind_speed,
+            "cloud_cover": cloud_cover
+        }
+        
+        result_list.append(result)
+        
+    return result_list
+            
+def get_five_days_weather(location_key):
+    url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}"
+    params = {"apikey": API_KEY, "details": "true", "metric": "true"}
     response = requests.get(url, params=params)
     if response.ok:
         weather_data = response.json()
-        return parse_weather_data(weather_data)
+        return parse_five_days_weather(weather_data)
     else:
         return {"error": True, "message": response.text, "code": response.status_code}
-
 
